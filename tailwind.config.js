bdcd3a996a95549b087cdf62269b059ffa7d79ca
ゆか（Yuka）San <128908897+yukazakiri@users.noreply.github.com>
2025-01-18 14:27:14 +0800

    Update environment configuration, package dependencies, and Tailwind CSS settings
    - Changed APP_URL in .env file to use a local testing URL.
    - Removed the bun.lockb file as it is no longer needed.
    - Added new dependencies: @jridgewell/gen-mapping and kleur, and updated lucide-react version in package.json and package-lock.json.
    - Enhanced Tailwind CSS configuration by adding new color and border radius settings for improved theming.
    - Updated app.css to reflect new CSS variable values for better design consistency.
    
    These changes improve the development environment and enhance the styling capabilities of the project.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,51 @@ export default {
     ],
     theme: {
         extend: {
-            borderColor: {
-                border: 'hsl(var(--border))'
+          colors: {
+            border: "hsl(var(--border))",
+            input: "hsl(var(--input))",
+            ring: "hsl(var(--ring))",
+            background: "hsl(var(--background))",
+            foreground: "hsl(var(--foreground))",
+            primary: {
+              DEFAULT: "hsl(var(--primary))",
+              foreground: "hsl(var(--primary-foreground))",
             },
-            backgroundColor: {
-                background: 'hsl(var(--background))'
+            secondary: {
+              DEFAULT: "hsl(var(--secondary))",
+              foreground: "hsl(var(--secondary-foreground))",
             },
-            textColor: {
-                foreground: 'hsl(var(--foreground))'
-            }
+            destructive: {
+              DEFAULT: "hsl(var(--destructive))",
+              foreground: "hsl(var(--destructive-foreground))",
+            },
+            muted: {
+              DEFAULT: "hsl(var(--muted))",
+              foreground: "hsl(var(--muted-foreground))",
+            },
+            accent: {
+              DEFAULT: "hsl(var(--accent))",
+              foreground: "hsl(var(--accent-foreground))",
+            },
+            popover: {
+              DEFAULT: "hsl(var(--popover))",
+              foreground: "hsl(var(--popover-foreground))",
+            },
+            card: {
+              DEFAULT: "hsl(var(--card))",
+              foreground: "hsl(var(--card-foreground))",
+            },
+          },
+          borderRadius: {
+            lg: `var(--radius)`,
+            md: `calc(var(--radius) - 2px)`,
+            sm: "calc(var(--radius) - 4px)",
+          },
         },
-    },
+      },
     plugins: [
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
+        require("tailwindcss-animate"),
     ],
 };
